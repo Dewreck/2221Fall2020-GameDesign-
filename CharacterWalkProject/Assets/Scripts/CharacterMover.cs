9df93d77c4fb93887bfde0bd8c31367c4da10b92
Derek <10873409@my.uvu.edu>
2020-10-05 15:53:05 -0600

    Added WalkProject & Gitignore

---
+++ CharacterWalkProject/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float walkSpeed = 20f, runSpeed = 50f;
+    private float speed;
+
+    public Rigidbody rBody;
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+        
+        float lateralMove = Input.GetAxis("Horizontal");
+        float vertMove = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(lateralMove, 0, vertMove) * (speed * Time.fixedDeltaTime);
+        rBody.MovePosition(transform.position + movement);
+    }
+}
---
+++ CharacterWalkProject/Assets/Scripts/CharacterWalk.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterWalk : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float fastSpeed, walkSpeed, gravity = -10;
+    private float yVar, speed;
+
+    private Vector3 movement;
+
+    private void Start()
+    {
+        speed = walkSpeed;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Walk();
+        
+        
+
+    }
+
+    private void Walk()
+    {
+        float vInput = Input.GetAxis("Vertical") * speed;
+        float hInput = Input.GetAxis("Horizontal") * speed;
+        movement = transform.forward * vInput + transform.right * hInput;
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
+    }
+}
