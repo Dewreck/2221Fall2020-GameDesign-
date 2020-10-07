4a6f0fa536ecb84aef74c7ce442953e2d0af61af
Derek <10873409@my.uvu.edu>
2020-10-06 22:35:31 -0600

    SingleJump Sandbox

---
+++ GameDesignWalkRunJumpSandbox/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class PlayerJump : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 velocity;
+    public float gravity = 10f, jumpPower = 10f;
+    private float yVar;
+
+    private void Update()
+    {
+        if (!controller.isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = jumpPower;
+        }
+    }
+}
